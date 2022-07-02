8876f6bbe3ddc3adbd5612b75c63889ce7627351
Piotr Polok <polok.piotr144@gmail.com>
2022-07-02 14:29:00 +0200

    Display metadata in main Program class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BitmapFileInfo = psiz_p_zd5.DTO.BitmapFileInfo;
+
+
+namespace psiz_p_zd5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first file path: ");
+
+            string fileName;
+            fileName = Console.ReadLine();
+            Console.WriteLine(Environment.NewLine + "Metadata: " + Environment.NewLine);
+
+            BitmapFileInfo bitmapFileInfo = new BitmapFileInfo(
+                BitmapFileHeaderReader.Read(fileName),
+                BitmapPictureHeaderReader.Read(fileName),
+                BitmapColorsHeaderReader.Read(fileName)
+                );
+
+            BitmapInfoConsolePrinter.Print(bitmapFileInfo);
+        }
+    }
+}
+
